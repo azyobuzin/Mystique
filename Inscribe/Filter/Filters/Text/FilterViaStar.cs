ec6b13d87b4e9a210971a41c96f5cafefd96bed6
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-04-07 13:05:03 +0900

    via* フィルタを作成

---
+++ Inscribe/Filter/Filters/Text/FilterViaStar.cs
@@ -0,0 +1,42 @@
+﻿using Dulcet.Twitter;
+using System.Linq;
+
+namespace Inscribe.Filter.Filters.Text
+{
+    public class FilterViaStar : FilterVia
+    {
+        private FilterViaStar() : base(null) { }
+
+        public FilterViaStar(string needle) : base(needle, false) { }
+
+        public FilterViaStar(string needle, bool isCaseSensitive) : base(needle, isCaseSensitive) { }
+
+        public override string Identifier
+        {
+            get { return base.Identifier + "*"; }
+        }
+
+        public override System.Collections.Generic.IEnumerable<string> Aliases
+        {
+            get { return base.Aliases.Select(s => s + "*"); }
+        }
+
+        public override string Description
+        {
+            get { return base.Description + "*"; }
+        }
+
+        public override string FilterStateString
+        {
+            get { return base.FilterStateString + "*"; }
+        }
+
+        protected override bool FilterStatus(TwitterStatusBase status)
+        {
+            var st = status as TwitterStatus;
+            return st != null && this.Match(
+                st.RetweetedOriginal != null ? st.RetweetedOriginal.Source : st.Source,
+                this.needle, this.isCaseSensitive);
+        }
+    }
+}
